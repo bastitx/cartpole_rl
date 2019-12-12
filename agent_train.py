fad0b0bc7da603614b50c8295c09800356165632
Bastian Harren <bastian@harren.co>
2019-12-12 23:30:00 +0100

    trained balancing pendulum

--- agent_train.py
+++ agent_train.py
@@ -6,43 +6,52 @@ import torch
 import sys
 
 def main():
+    global env, agent, epoch
     env = InvertedPendulumEnv()
-    global agent
 
     env.seed(0)
-    agent = DDPGAgent(env.observation_space, env.action_space, ActorModel, CriticModel)
+    agent = DDPGAgent(env.observation_space, env.action_space, ActorModel, CriticModel, epsilon=0.2)
+    epoch = 2299
     try:
-        agent.load_weights('models')
+        agent.load_weights('models', epoch)
         print("loaded weights")
     except Exception:
         print("Couldn't load weights!")
-    episode_count = 500
-    max_episode_length = 400
+    max_iterations = 100000
+    last_epoch_start = -1
+    max_epoch_length = 400
+    epoch_reward = 0
+    
+    done = True
 
-    for i in range(episode_count):
-        state = env.reset()
-        episode_reward = 0
-        for j in range(max_episode_length):
-            #env.render()
-            action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
-            #print(state[1], action[0], reward)
-            episode_reward += reward
-            agent.remember([state], [action], [next_state], [reward], [done])
-            agent.update()
-            state = next_state
-            if done:
-                break
-        print("{}: avg.reward: {}, length: {}, epsilon: {}".format(i, episode_reward/(j+1), j+1, agent.epsilon))
+    for i in range(max_iterations):
+        if done:
+            epoch += 1
+            epoch_length = i - last_epoch_start
+            print("{}/{}: avg.reward: {}, length: {}, epsilon: {}".format(i,max_iterations, epoch_reward/epoch_length, epoch_length, agent.epsilon))
+            state = env.reset()
+            epoch_reward = 0
+            last_epoch_start = i
+        #env.render()
+        action = agent.act(state)
+        next_state, reward, done, _ = env.step(action)
+        #print(state[1], action[0], reward)
+        epoch_reward += reward
+        agent.remember([state], [action], [next_state], [reward], [done])
+        agent.update()
+        state = next_state
+        if i - last_epoch_start >= max_epoch_length:
+            done = True
         
     env.close()
-    agent.save_model('models')
+    agent.save_model('models', epoch)
 
 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        agent.save_model('models')
+        env.close()
+        agent.save_model('models', epoch)
         print("Saved model")
     sys.exit(0)
--- ddpg_agent.py
+++ ddpg_agent.py
@@ -8,7 +8,7 @@ from random_process import OrnsteinUhlenbeckProcess
 
 class DDPGAgent():
     def __init__(self, state_space, action_space, ActorModel, CriticModel, gamma=0.99, epsilon=1.0, epsilon_min=0.1,
-                    epsilon_decay=0.9999, lr_actor=0.0001, lr_critic=0.001, tau=0.001, batch_size=64):
+                    epsilon_decay=0.99995, lr_actor=0.0001, lr_critic=0.001, tau=0.001, batch_size=64):
         self.action_space = action_space
         self.state_space = state_space
         self.gamma = gamma #discount
@@ -25,16 +25,16 @@ class DDPGAgent():
         self.critic = CriticModel(self.state_space, self.action_space)
         self.critic_target = CriticModel(self.state_space, self.action_space)
         self.optim_actor = torch.optim.Adam(self.actor.parameters(), lr=self.lr_actor)
-        self.optim_critic = torch.optim.Adam(self.critic.parameters(), lr=self.lr_critic, weight_decay=0.01)
-        self.memory = ReplayMemory(600000)
+        self.optim_critic = torch.optim.Adam(self.critic.parameters(), lr=self.lr_critic)#, weight_decay=0.01)
+        self.memory = ReplayMemory(50000)
 
         hard_update(self.actor_target, self.actor)
         hard_update(self.critic_target, self.critic)
     
     def act(self, state):
         action = self.actor(torch.tensor(state).float()).detach().numpy()
-        #action += self.epsilon * self.random_process.sample()
-        action += np.random.normal(scale=self.epsilon)
+        action += self.epsilon * self.random_process.sample()
+        #action += np.random.normal(scale=self.epsilon)
         action = np.clip(action, -1., 1.)
         self.epsilon = max(self.epsilon*self.epsilon_decay, self.epsilon_min)
         return action
@@ -56,7 +56,7 @@ class DDPGAgent():
         with torch.no_grad():
             next_q_values = self.critic_target((torch.tensor(next_state_batch).float(), self.actor_target(torch.tensor(next_state_batch).float())))
 
-        q_target_batch = reward_batch + self.gamma * done_batch * next_q_values
+        q_target_batch = reward_batch + self.gamma * (1-done_batch) * next_q_values
         
         self.critic.zero_grad()
         q_batch = self.critic((state_batch, action_batch))
@@ -65,33 +65,33 @@ class DDPGAgent():
         self.optim_critic.step()
 
         self.actor.zero_grad()
-        policy_loss = -self.critic((state_batch, self.actor(state_batch)))
-        policy_loss = policy_loss.mean().backward()
+        policy_loss = -self.critic((state_batch, self.actor(state_batch))).mean()
+        policy_loss.backward()
         self.optim_actor.step()
 
         soft_update(self.actor_target, self.actor, self.tau)
         soft_update(self.critic_target, self.critic, self.tau)
     
-    def load_weights(self, output):
+    def load_weights(self, output, epoch):
         if output is None: return
 
         self.actor.load_state_dict(
-            torch.load('{}/actor.pkl'.format(output))
+            torch.load('{}/actor-{}.pkl'.format(output, epoch))
         )
 
         self.critic.load_state_dict(
-            torch.load('{}/critic.pkl'.format(output))
+            torch.load('{}/critic-{}.pkl'.format(output, epoch))
         )
 
 
-    def save_model(self,output):
+    def save_model(self,output, epoch):
         torch.save(
             self.actor.state_dict(),
-            '{}/actor.pkl'.format(output)
+            '{}/actor-{}.pkl'.format(output, epoch)
         )
         torch.save(
             self.critic.state_dict(),
-            '{}/critic.pkl'.format(output)
+            '{}/critic-{}.pkl'.format(output, epoch)
         )
 
         
