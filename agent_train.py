a783f679ed00e9459f4176f4ba7cf85a635255a7
Bastian Harren <bastian@harren.co>
2019-12-17 14:44:36 +0100

    added parser

--- agent_train.py
+++ agent_train.py
@@ -4,51 +4,70 @@ from inverted_pendulum import InvertedPendulumEnv
 from model import ActorModel, CriticModel
 import torch
 import sys
+import argparse
 
 def main():
-    global env, agent, episode, train
+    global env, agent, args, episode
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--mode', default='train', type=str)
+    parser.add_argument('--max_iterations', default=100000, type=int)
+    parser.add_argument('--warmup', default=128, type=int)
+    parser.add_argument('--max_episode_length', default=500, type=int)
+    parser.add_argument('--resume_episode', default=0, type=int)
+    parser.add_argument('--epsilon', default=1.0, type=float)
+    parser.add_argument('--epsilon_decay', default=0.9999, type=float)
+    parser.add_argument('--epsilon_min', default=0.1, type=float)
+    parser.add_argument('--gamma', default=0.99, type=float)
+    parser.add_argument('--lr_actor', default=0.0001, type=float)
+    parser.add_argument('--lr_critic', default=0.001, type=float)
+    parser.add_argument('--tau', default=0.001, type=float)
+    parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--memory_size', default=10000, type=int)
+
+    args = parser.parse_args()
+
     env = InvertedPendulumEnv()
-    train = True
-    max_iterations = 100000
-    warmup = 128
-    max_episode_length = 100
-    episode = 0
 
     env.seed(0)
-    agent = DDPGAgent(env.observation_space, env.action_space, ActorModel, CriticModel, epsilon=1.0)
-    try:
-        agent.load_weights('models', episode, not train)
-        print("loaded weights")
-    except Exception:
-        print("Couldn't load weights!")
-    
+    agent = DDPGAgent(env.observation_space, env.action_space, ActorModel, CriticModel, args.gamma, args.epsilon, args.epsilon_min,
+                    args.epsilon_decay, args.lr_actor, args.lr_critic, args.tau, args.batch_size, args.memory_size)
+    if args.resume_episode > 0:
+        try:
+            agent.load_weights('models', args.resume_episode, args.mode is not 'train')
+            print("loaded weights")
+        except Exception:
+            print("Couldn't load weights!")
+
+    episode = args.resume_episode
     episode_reward = 0
     last_episode_start = -1
     done = True
 
-    for i in range(max_iterations):
+    for i in range(args.max_iterations):
         if done:
             episode += 1
             episode_length = i - last_episode_start
-            print("{}/{}: episode: {}, avg.reward: {:.3f}, length: {}, epsilon: {:.3f}".format(i,max_iterations, episode, episode_reward/episode_length, episode_length, agent.epsilon))
+            print("{}/{}: episode: {}, avg.reward: {:.3f}, length: {}, epsilon: {:.3f}".format(i,args.max_iterations, episode, episode_reward/episode_length, episode_length, agent.epsilon))
             state = env.reset()
             episode_reward = 0
             last_episode_start = i
-        if not train:
+        if args.mode is not 'train':
             env.render()
         action = agent.act(state)
         next_state, reward, done, _ = env.step(action)
         #print(state[1], action[0], reward)
         episode_reward += reward
         agent.remember([state], [action], [next_state], [reward], [done])
-        if train and i >= warmup:
+        if args.mode is 'train' and i >= args.warmup:
             agent.update()
         state = next_state
-        if i - last_episode_start >= max_episode_length:
+        if i - last_episode_start >= args.max_episode_length:
             done = True
         
     env.close()
-    if train:
+    if args.mode is 'train':
         agent.save_model('models', episode)
         print("Saved model")
 
@@ -58,7 +77,7 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         env.close()
-        if train:
+        if args.mode is 'train':
             agent.save_model('models', episode)
             print("Saved model")
     sys.exit(0)
