33c6a25687ac82342c4a8eb22e7b85263122e9d3
Bastian Harren <bastian@harren.co>
2020-02-11 12:54:39 +0100

    randomized mujoco environment

--- inverted_pendulum.py
+++ inverted_pendulum.py
@@ -1,11 +1,59 @@
 import numpy as np
-from gym import utils
-from gym.envs.mujoco import mujoco_env
+from gym import utils, spaces
+import mujoco_env
+import re
+
+XML = '''
+<mujoco model="inverted pendulum">
+	<compiler inertiafromgeom="true"/>
+	<default>
+		<joint armature="0" limited="true"/>
+		<geom contype="0" rgba="0.7 0.7 0 1"/>
+		<tendon/>
+	</default>
+	<option gravity="0 0 -9.81" integrator="RK4" timestep="0.02"/>
+	<size nstack="3000"/>
+	<worldbody>
+		<geom name="rail" pos="0 0 0" quat="0.707 0 0.707 0" friction="1 0.1 0.1" rgba="0.3 0.3 0.7 1" size="0.02 2" type="capsule"/>
+		<body name="cart" pos="0 0 0">
+			<joint axis="1 0 0" limited="true" name="slider" damping="{slider_damping}" pos="0 0 0" range="-2 2" type="slide"/>
+			<geom name="cart" pos="0 0 0" mass="{cart_mass}" quat="0.707 0 0.707 0" size="0.1 0.1" type="capsule"/>
+			<body name="pole" pos="0 0 0">
+				<joint axis="0 1 0" name="hinge" damping="{hinge_damping}" pos="0 0 0" limited="false" type="hinge"/>
+				<geom fromto="0 0 0 0.001 0 -{pole_length}" friction="1 0.1 0.1" name="cpole" rgba="0 0.7 0.7 1" size="0.049 0.4" type="capsule" mass="{pole_mass}"/>
+			</body>
+		</body>
+	</worldbody>
+	<actuator>
+		<general gear="{motor_gear}" gainprm="{motor_gain} 0 0"  joint="slider" name="slide" ctrlrange="-3 3"/>
+	</actuator>
+</mujoco>
+'''
+#add frictionloss to joints?
+
+slider_damping=0.5 #0
+hinge_damping=0.5 #1
+motor_gear=200 #2
+motor_gain=1 #3
+cart_mass=10 #4
+pole_length=0.6 #5
+pole_mass=5 #6
+param_space=spaces.Box(np.array([0.01, 0.01, 20, 0.1, 1, 0.1, 1]), np.array([3, 3, 500, 2, 100, 20, 20]))
 
 class InvertedPendulumEnv(mujoco_env.MujocoEnv, utils.EzPickle):
     def __init__(self):
         utils.EzPickle.__init__(self)
-        mujoco_env.MujocoEnv.__init__(self, '/Users/bastian/Development/master/cartpole_rl/inverted_pendulum.xml', 2)
+        params = param_space.sample()
+        xml_str = XML.format(
+            slider_damping=params[0],
+            hinge_damping=params[1],
+            motor_gear=params[2],
+            motor_gain=params[3],
+            cart_mass=params[4],
+            pole_length=params[5],
+            pole_mass=params[6]
+        )
+        mujoco_env.MujocoEnv.__init__(self, xml_str, 2)
 
     def step(self, a):
         self.do_simulation(a, self.frame_skip)
