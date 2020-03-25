eae8fea2cef0787e512185fbeb0a675e40af611b
Bastian Harren <bastian@harren.co>
2020-03-25 15:35:12 +0100

    train model via stable_baselines library

---
+++ baseline_train.py
@@ -0,0 +1,25 @@
+import gym
+from sim.cartpole import CartPoleEnv
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines import PPO2
+
+env = CartPoleEnv(swingup=False, randomize=False)
+# Optional: PPO2 requires a vectorized environment to run
+# the env is now wrapped automatically when passing it to the constructor
+# env = DummyVecEnv([lambda: env])
+
+model = PPO2(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=10000)
+
+obs = env.reset()
+dones = False
+for i in range(1000):
+    action, _states = model.predict(obs)
+    if dones:
+        obs = env.reset()
+        dones = False
+    else:
+        obs, rewards, dones, info = env.step(action)
+    env.render()
+
+env.close()
