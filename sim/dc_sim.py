acc7439286647c9c7c74d7f12a958fac50831c3a
Bastian Harren <bastian@harren.co>
2020-04-19 11:06:26 +0200

    try something..

--- sim/dc_sim.py
+++ sim/dc_sim.py
@@ -22,7 +22,7 @@ class DCMotorSim():
         states = torch.tensor(states, device=device).detach()
         states_mean = states.mean(0)
         states_std = states.std(0)
-        states = (states - states_mean) / states_std
+        #states = (states - states_mean) / states_std
         actions = torch.tensor(actions, device=device).detach()
         weights = torch.tensor([1., 0.25, 0.2, 0.0004], device=device).detach()
         for _ in range(epochs):
@@ -34,10 +34,10 @@ class DCMotorSim():
                 comp_state = torch.stack([torch.cat((states[j-5:j].flatten(), actions[j-5:j])) for j in range(i, i + batch_size)])
                 force = self.model(comp_state)
                 state, *_ = env.step(force)
-                state = (state - states_mean) / states_std
-                res = (states[i:i+batch_size] - state).mv(weights)
+                #state = (state - states_mean) / states_std
+                res = (states[i:i+batch_size] - state).pow(2).mv(weights)
                 for j in range(batch_size // mini_batch_size):
-                    loss = res[j*mini_batch_size:(j+1)*mini_batch_size].pow(2).mean() # try abs() instead of pow(2)
+                    loss = res[j*mini_batch_size:(j+1)*mini_batch_size].mean() # try abs() instead of pow(2)
                     #print("Loss: {}".format(loss))
                     epoch_loss += [loss.detach()]
                     optim.zero_grad()
@@ -56,6 +56,6 @@ if __name__ == "__main__":
     from sim.cartpole_train_dc import CartPoleEnv
     from model import DCModel
     data = read_data('data.csv')
-    sim = DCMotorSim(DCModel, filename=None)
+    sim = DCMotorSim(DCModel, filename='dc_model_old.pkl')
     env = CartPoleEnv(swingup=False)
-    sim.train(data, env, 100, batch_size=128)
+    sim.train(data, env, 100, batch_size=64, lr=0.0001)
