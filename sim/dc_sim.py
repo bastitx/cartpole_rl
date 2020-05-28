12d960b407df01ef02525e3e28ef07d3a0957db9
Bastian Harren <bastian@harren.co>
2020-05-28 16:24:53 +0200

    added batch_step

--- sim/dc_sim.py
+++ sim/dc_sim.py
@@ -6,7 +6,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 class DCMotorSim():
     def __init__(self, Model, filename=None, num_states=5):
         self.num_states = num_states # 4 state + 1 action
-        self.model = Model(self.num_states*5, 1, p=0.1).to(device)
+        self.model = Model(self.num_states*5, 1, p=0.0).to(device)
         self.model.eval()
         if filename != None:
             self.model.load_state_dict(torch.load(filename, map_location=device))
@@ -14,7 +14,7 @@ class DCMotorSim():
     def step(self, action):
         return self.model(action.detach())
 
-    def train(self, data, env, epochs=10, steps=100, batch_size=512, mini_batch_size=None, lr=0.001):
+    def train(self, data, env, epochs=10, steps=100, batch_size=512, mini_batch_size=None, batch_step=1, lr=0.001):
         assert(mini_batch_size == None or (batch_size % mini_batch_size == 0 and mini_batch_size < batch_size))
         if mini_batch_size == None:
             mini_batch_size = batch_size
@@ -26,22 +26,22 @@ class DCMotorSim():
         #states_std = states.std(0)
         #states = (states - states_mean) / states_std
         actions = torch.tensor(actions, device=device).detach()
-        weights = torch.tensor([1., 0.25, 0.2, 0.0004], device=device).detach()
+        weights = torch.tensor([1., 0.25, 0.0, 0.0000], device=device).detach()
         for _ in range(epochs):
             epoch_loss = []
-            for i in range(self.num_states, len(states) - steps - (len(states) % batch_size), batch_size):
+            for i in range(self.num_states, len(states) - steps - (batch_size-1)*batch_step - (len(states) % batch_size), batch_size*batch_step):
                 env.reset()
                 self.model.reset(batch_size)
-                env.state = states[i-1:i-1+batch_size]
+                env.state = states[i-1:i-1+batch_size*batch_step:batch_step]
                 res = torch.zeros(batch_size).to(device)
-                comp_state = torch.stack([torch.cat((states[j-self.num_states:j].flatten(), actions[j-self.num_states:j])) for j in range(i, i + batch_size)])
+                comp_state = torch.stack([torch.cat((states[j-self.num_states:j].flatten(), actions[j-self.num_states:j])) for j in range(i, i + batch_size*batch_step, batch_step)])
                 for k in range(steps):
                     force = self.model(comp_state)
                     state, *_ = env.step(force)
                     #state = (state - states_mean) / states_std
-                    comp_state = torch.stack([torch.cat((s[:(self.num_states-1)*4], state[j], actions[i+j+k-self.num_states+1:i+j+k+1])) for j, s in enumerate(comp_state)])
-                    res += (states[i+k:i+k+batch_size] - state).mv(weights).pow(2)
-                #res = (states[i+steps:i+steps+batch_size] - state).mv(weights).pow(2) 
+                    comp_state = torch.stack([torch.cat((s[:(self.num_states-1)*4], state[j], actions[i+j*batch_step+k-self.num_states+1:i+j*batch_step+k+1])) for j, s in enumerate(comp_state)])
+                    res += (states[i+k:i+k+batch_size*batch_step:batch_step] - state).mv(weights).pow(2)
+                #res = (states[i+steps:i+steps+batch_size*batch_step:batch_step] - state).mv(weights).pow(2)
                 for j in range(batch_size // mini_batch_size):
                     loss = res[j*mini_batch_size:(j+1)*mini_batch_size].mean() # try abs() instead of pow(2)
                     #print("Loss: {}".format(loss))
@@ -62,10 +62,10 @@ if __name__ == "__main__":
     from sim.cartpole import CartPoleEnv
     from model import DCModel
     data = read_data('demonstration__2020-05-19__10-49-45.csv')
-    sim = DCMotorSim(DCModel, None, 1)
+    sim = DCMotorSim(DCModel, "dc_model_old.pkl", 1)
     env = CartPoleEnv(swingup=True)
     try:
-        sim.train(data, env, 50, batch_size=1, steps=16712, mini_batch_size=None, lr=0.00001)
+        sim.train(data, env, 100, batch_size=512, steps=1, mini_batch_size=None, batch_step=1, lr=0.0000001)
     except KeyboardInterrupt:
         torch.save(sim.model.state_dict(), "dc_model_aborted.pkl")
     sys.exit(0)
